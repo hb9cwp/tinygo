f5ef5a3bf34366b05b7b4e01da636ad18882d2a3
Rolf Sommerhalder <rolf.sommerhalder@alumni.ethz.ch>
2025-07-01 13:11:23 +0200

    first go at Overclocking

--- go.mod
+++ go.mod
@@ -1,37 +1,39 @@
 module github.com/tinygo-org/tinygo
 
-go 1.22.0
+go 1.23.0
+
+toolchain go1.24.2
 
 require (
 	github.com/aykevl/go-wasm v0.0.2-0.20240825160117-b76c3f9f0982
-	github.com/blakesmith/ar v0.0.0-20150311145944-8bd4349a67f2
+	github.com/blakesmith/ar v0.0.0-20190502131153-809d4375e1fb
 	github.com/chromedp/cdproto v0.0.0-20220113222801-0725d94bb6ee
 	github.com/chromedp/chromedp v0.7.6
-	github.com/gofrs/flock v0.8.1
-	github.com/google/shlex v0.0.0-20181106134648-c34317bd91bf
+	github.com/gofrs/flock v0.12.1
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
 	github.com/inhies/go-bytesize v0.0.0-20220417184213-4913239db9cf
-	github.com/marcinbor85/gohex v0.0.0-20200531091804-343a4b548892
-	github.com/mattn/go-colorable v0.1.13
-	github.com/mattn/go-tty v0.0.4
-	github.com/sigurn/crc16 v0.0.0-20211026045750-20ab5afb07e3
+	github.com/marcinbor85/gohex v0.0.0-20210308104911-55fb1c624d84
+	github.com/mattn/go-colorable v0.1.14
+	github.com/mattn/go-tty v0.0.7
+	github.com/sigurn/crc16 v0.0.0-20240131213347-83fcde1e29d1
 	github.com/tetratelabs/wazero v1.6.0
-	go.bug.st/serial v1.6.0
-	golang.org/x/net v0.35.0
-	golang.org/x/sys v0.30.0
-	golang.org/x/tools v0.30.0
+	go.bug.st/serial v1.6.4
+	golang.org/x/net v0.40.0
+	golang.org/x/sys v0.33.0
+	golang.org/x/tools v0.33.0
 	gopkg.in/yaml.v2 v2.4.0
 	tinygo.org/x/go-llvm v0.0.0-20250422114502-b8f170971e74
 )
 
 require (
 	github.com/chromedp/sysutil v1.0.0 // indirect
-	github.com/creack/goselect v0.1.2 // indirect
+	github.com/creack/goselect v0.1.3 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 )
--- src/machine/machine_rp2_clocks.go
+++ src/machine/machine_rp2_clocks.go
@@ -13,6 +13,154 @@ func CPUFrequency() uint32 {
 	return cpuFreq
 }
 
+func SetCPUFrequency(cpuFreq uint64) uint32 {
+	// XXX see line 203 in machine_rp2_clocks.go
+	//  clkSys = pllSys (125MHz) / 1 = 125MHz
+	//  csys := clks.clock(clkSys)
+	// ...
+	// Compute clock dividers
+	// from init()
+	//  https://github.com/tinygo-org/tinygo/blob/3e60eeb368f25f237a512e7553fd6d70f36dc74c/src/machine/machine_rp2_clocks.go#L180
+	//      xosc   revdiv   REF    FBDIV   VCO     POSTDIV
+	// pllSys: 12 / 1 =    12 MHz * 125 = 1500 MHZ / 6 / 2 = 125 MHz
+	// pllUSB: 12 / 1 =    12 MHz * 40  =  480 MHz / 5 / 2 =  48 MHz
+	//fb, revdiv, pd1, pd2, err := pllSearch{LockRefDiv: 1}.CalcDivs(xoscFreq*MHz, cpuFreq*MHz, MHz)
+	fb, _, pd1, pd2, err := pllSearch{LockRefDiv: 1}.CalcDivs(xoscFreq*MHz, cpuFreq*MHz, MHz)
+	if err != nil {
+		panic(err)
+	}
+	pllsysFB, pllsysPD1, pllsysPD2 = uint32(fb), uint32(pd1), uint32(pd2)
+
+	// from RPi SDK Repo: pll_init()
+	//  https://github.com/raspberrypi/pico-sdk/blob/ee68c78d0afae2b69c03ae1a72bf5cc267a2d94c/src/rp2_common/hardware_pll/include/hardware/pll.h#L54
+	// pll		pll_sys or pll_usb
+ 	// ref_div	Input clock divider
+ 	// vco_freq	Requested output from the VCO (voltage controlled oscillator)
+ 	// post_div1	Post Divider 1 - range 1-7. Must be >= post_div2
+ 	// post_div2	Post Divider 2 - range 1-7
+	// see also pico-sdk/src/rp2_common/hardware_clocks/include/hardware/clocks.h
+	//  https://github.com/raspberrypi/pico-sdk/blob/ee68c78d0afae2b69c03ae1a72bf5cc267a2d94c/src/rp2_common/hardware_clocks/include/hardware/clocks.h#L156C30-L156C36
+	//pllsysFB, pllsysPD1, pllsysPD2 = 125, 6, 2	// 125 MHz  default PLL config on RP2040 "standard"
+	//pllsysFB, pllsysPD1, pllsysPD2 = 125, 5, 2 	// 150 MHz  default PLL config on RP2350 "standard"
+	//pllsysFB, pllsysPD1, pllsysPD2 =  73, 5, 1	// 175 MHz
+	//pllsysFB, pllsysPD1, pllsysPD2 = 100, 6, 1	// 200 MHz  default PLL config on RP2040 "fast"
+	//pllsysFB, pllsysPD1, pllsysPD2 = 125, 6, 1	// 250 MHz
+	println("pllsysFB= ", pllsysFB, "   pllsysPD1= ", pllsysPD1, "   pllsysPD2= ", pllsysPD2)
+
+	pllSys.init(1, pllsysFB, pllsysPD1, pllsysPD2)
+	//return cpuFreq
+	return xoscFreq / 1 * xoscFreq / pllsysPD1 / pllsysPD2
+}
+
+func FrequencyCount(cix uint32) uint32 {
+	// takes the source as an argument and returns the frequency in kHz:
+	// from RP2350 Datasheet, 8.1.5.2. Using the frequency counter
+	// The SDK defines a frequency_count function in lines 147 - 174
+	// https://github.com/raspberrypi/pico-sdk/blob/master/src/rp2_common/hardware_clocks/clocks.c
+
+	//var fc0 := fc(src: cix)	// clock index		// see fc0 in struct clocksType of machine_rp2_2350.go#L80
+	//	var clks *clocksType	// XXX
+	//csys := clks.clock(clkSys)
+	//var ct *rp.CLOCKS_Type		// tinygo/src/device/rp2350.go (auto generated)
+	//var clks *clocksType	// XXX
+	//ct := clks.fc0
+
+	// If frequency counter is running need to wait for it. It runs even if the source is NULL
+	// tinygo/src/device/rp2350.go
+	// type CLOCKS_Type struct {
+	//   FC0_STATUS           volatile.Register32 // 0xA4
+	//
+	//	for !clks.fc0.status.HasBits(0x1 << 4) {	// DONE: Test complete
+	//		_ = tight_loop_contents(77, 123)
+	//	}
+//	for !ct.GetFC0_STATUS_DONE() {
+//	for ct.GetFC0_STATUS_DONE() != 1 {
+//	for !ct.GetFC0_STATUS_DONE() {
+//	for !clocks.fc0.GetFC0_STATUS_DONE() {
+	// func (o *CLOCKS_Type) GetFC0_STATUS_DONE() uint32 {		// from src/device/rp2350.go
+        //   return (volatile.LoadUint32(&o.FC0_STATUS.Reg) & 0x10) >> 4
+	// }
+	//for !((volatile.LoadUint32(&clocks.fc0.status) & 0x10) >> 4) {
+	//for !((clocks.fc0.status & 0x10) >> 4) {
+	for !(clocks.fc0.status.HasBits(0x10)) {
+		_ = tight_loop_contents(77, 123)
+		//_ = tight_loop_contents(77, 123, "-")
+	}
+
+	// Set reference freq
+	// fc->ref_khz = clock_get_hz(clk_ref) / 1000;
+	//ct.SetFC0_REF_KHZ(12000000 / 1000) // XXX
+	clocks.fc0.refKHz.Set(12000000 / 1000)
+
+	// FIXME: Don't pick random interval. Use best interval
+	// fc->interval = 10;
+	// 1us * 2**interval with default interval = 8 gives a test interval of 250us
+	// 4 Bit, see RP2350 Datasheet p. 543
+	//ct.SetFC0_INTERVAL(10) 	// 2^10 * 1 us = 1 ms
+	clocks.fc0.interval.Set(10)	// 2^10 * 1 us = 1 ms
+
+	// XXX No min or max
+	// fc->min_khz = 0;
+	// fc->max_khz = 0xffffffff;	<= example differs from auto generated device/rp2350.go!
+	// Minimum pass frequency in kHz. This is optional.
+	// Set to 0 if you are not using the pass/fail flags
+	//ct.SetFC0_MIN_KHZ(0)
+	clocks.fc0.minKHz.Set(0)
+	// Maximum pass frequency in kHz. This is optional.
+	// Set to 0x1ffffff if you are not using the pass/fail flags
+	//ct.SetFC0_MAX_KHZ(0x1ffffff)
+	clocks.fc0.maxKHz.Set(0x1ffffff)
+
+	// Set SRC which automatically starts the measurement
+	// fc->src = src;
+	// tinygo/src/device/rp2350.go
+	// type CLOCKS_Type struct {
+	//   FC0_SRC              volatile.Register32 // 0xA0
+	//	clks.fc0.src = cix
+	// Clock sent to frequency counter, set to 0 when not required
+	// Writing to this register initiates the frequency count
+	//ct.SetFC0_SRC(cix)
+	clocks.fc0.src.Set(cix)
+
+	// while(!(fc->status & CLOCKS_FC0_STATUS_DONE_BITS)) {
+	//for ct.GetFC0_STATUS_DONE() != 1 {
+	for !(clocks.fc0.status.HasBits(0x10)) {
+		_ = tight_loop_contents(77, 123)
+		//_ = tight_loop_contents(77, 123, "+")
+	}
+
+	// Return the result
+	// return fc->result >> CLOCKS_FC0_RESULT_KHZ_LSB;
+	//	return fc0.result
+	//return ct.GetFC0_RESULT_KHZ()
+	return (clocks.fc0.result.Get() & 0x3fffffe0) >> 5
+}
+
+func tight_loop_contents(a, b uint) uint {
+//func tight_loop_contents(a, b uint, r rune) uint {
+//func tight_loop_contents(a, b uint, s string) uint {
+	// explanation:
+	// https://forums.raspberrypi.com/viewtopic.php?t=349804
+	// implementation from RP SDK:
+	// https://github.com/raspberrypi/pico-sdk/blob/f396d05f8252d4670d4ea05c8b7ac938ef0cd381/src/rp2_common/pico_platform/include/pico/platform.h#L358
+	// No-op function intended to be called by any tight hardware polling loop. Using this ubiquitously
+	// makes it much easier to find tight loops, but also in the future \#ifdef-ed support for lockup
+	// debugging might be added
+	//
+	// * This multiplies a by b using multiply instruction using the ARM mul instruction regardless of values (the compiler
+	// * might otherwise choose to perform shifts/adds), i.e. this is a 1 cycle operation.
+	// __force_inline static int32_t __mul_instruction(int32_t a, int32_t b) {
+	//   asm ("mul %0, %1" : "+l" (a) : "l" (b) : );
+	//   return a;		// returns (a * b)
+	// }
+	//print("w")
+	//print(r)
+	//print(s)
+	// XXX inline assmbly for ARM, see tinygo/src/device/
+	// https://tinygo.org/docs/concepts/compiler-internals/inline-assembly/
+	return a * b
+}
+
 // clockIndex identifies a hardware clock
 type clockIndex uint8
 
